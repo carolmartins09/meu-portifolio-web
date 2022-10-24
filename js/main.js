07f615ba943ff7c84c4f6ac90beaa6d6e8f06c1e
Carol Martins <carolmartins09@gmail.com>
2022-10-24 19:27:48 -0300

    finalização da seção interesses

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 let iconesInteresses = document.querySelectorAll(".icone");
+let divInteresses = document.querySelector("#caixa-textos");
+let botaoFecharTexto = document.querySelector(".fechar-texto");
+let texto = document.querySelector("#texto-interesses");
 
 for (let i = 0; i < iconesInteresses.length; i++) {
 
@@ -8,47 +11,24 @@ for (let i = 0; i < iconesInteresses.length; i++) {
     botao.onclick = function() {
         mostraTexto(idTextosInteresses)
     }
-
 }
 
 let descricoesInteresses = {
     "texto-gatinhos": "Tenho filhos felinos e atualmente vivem comigo o Zyon, o Iron e a Aurora, além do Snow, meu primeiro gatinho que já virou estrelinha. Eles são minha maior paixão.",
-    "texto-estrelas": "Sou apaixonada por assuntos relacionados a ciência, principalmente astrofísica e muito fã de Carl Sagan. Leio muitos livros sobre o assunto e meu preferido é 'O mundo assombrado pelos demônios', do qual ele é o autor. Tenho uma tatuagem com a frase <span style = 'font-style: italic;'> We are made of stars stuff</span>.",
-    "texto-gamer": "olá",
-    "texto-naruto": "Acompanho animes desde de criança, Naruto, Attack on Titan e Demon Slayer são meus preferidos.",
+    "texto-estrelas": "Sou apaixonada por assuntos relacionados a ciência, principalmente astrofísica. Meu livro preferido é 'O mundo assombrado pelos demônios', de Carl Sagan.",
+    "texto-gamer": "Adoro acompanhar campeonatos de eSport, principalmente de Counter-Strike.",
+    "texto-naruto": "Naruto, Attack on Titan e Demon Slayer são meus animes preferidos, e o que mais gosto de assistir nas horas vagas.",
     "texto-planta": "Além de mãe de pet, também sou mãe de planta e vivo na eterna luta de fazer as verdinhas sobreviverem em um apartamento.",
-    "texto-yoga":"Pratico yoga a alguns anos e é minha atividade"
+    "texto-yoga":"Pratico yoga a alguns anos e é minha atividade física preferida. É onde encontro paz na correria do dia-a-dia."
 }
 
 function mostraTexto(idTextosInteresses) {
-    
-    let texto = document.querySelector(".texto-interesses");
-    let divInteresses = document.querySelector(".caixa-textos");
-
     divInteresses.classList.remove("interesses-textos");
 
-    texto.innerHTML = descricoesInteresses(idTextosInteresses) || " ";
-
+    texto.innerHTML = descricoesInteresses[idTextosInteresses] || " ";
 }
 
+botaoFecharTexto.onclick = function() {
+    divInteresses.classList.add("interesses-textos");
+}
 
-
-
-// function mostraTexto (idTextosInteresses){
-
-//     let textosInteresses = document.querySelector(idTextosInteresses);
-
-//     textosInteresses.classList.toggle("display-block");
-// }
-
-
-// for (let i = 0; i < iconesInteresses.length; i++) {
-
-//     const botao = iconesInteresses[i];
-//     const icone = botao.classList[0]
-//     const idTextosInteresses = `#texto-${icone}`;
-
-//     botao.onclick = function () {
-//         mostraTexto(idTextosInteresses);
-//     }
-// }
